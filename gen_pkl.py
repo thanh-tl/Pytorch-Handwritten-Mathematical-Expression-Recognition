cd074da75f40f2858167f0ca0c2a9c71775eb813
thanhtl <thanhtl@whypay.vn>
2019-12-31 10:05:57 +0700

    gen pkl test and train

--- gen_pkl.py
+++ gen_pkl.py
@@ -10,7 +10,9 @@ import pickle as pkl
 import numpy
 from scipy.misc import imread, imresize, imsave
 
-image_path='C:\\Users\\msraspeech\\PycharmProjects\\v-honwa\\off_image_test\\off_image_test\\'
+print("GEN PKL TEST......");
+
+image_path='./off_image_test/'
 outFile='offline-test.pkl'
 oupFp_feature=open(outFile,'wb')
 
@@ -20,7 +22,7 @@ channels=1
 
 sentNum=0
 
-scpFile=open('C:\\Users\\msraspeech\\PycharmProjects\\v-honwa\\test_caption.txt')
+scpFile=open('./test_caption.txt')
 while 1:
     line=scpFile.readline().strip() # remove the '\r\n'
     if not line:
@@ -44,3 +46,41 @@ print('load images done. sentence number ',sentNum)
 pkl.dump(features,oupFp_feature)
 print('save file done')
 oupFp_feature.close()
+
+
+print("GEN PKL TRAIN......");
+image_path='./off_image_train/'
+outFile='offline-train.pkl'
+oupFp_feature=open(outFile,'wb')
+
+features={}
+
+channels=1
+
+sentNum=0
+
+scpFile=open('./train_caption.txt')
+while 1:
+    line=scpFile.readline().strip() # remove the '\r\n'
+    if not line:
+        break
+    else:
+        key = line.split('\t')[0]
+        image_file = image_path + key + '_' + str(0) + '.bmp'
+        im = imread(image_file)
+        mat = numpy.zeros([channels, im.shape[0], im.shape[1]], dtype='uint8')
+        for channel in range(channels):
+            image_file = image_path + key + '_' + str(channel) + '.bmp'
+            im = imread(image_file)
+            mat[channel,:,:] = im
+        sentNum = sentNum + 1
+        features[key] = mat
+        if sentNum / 500 == sentNum * 1.0 / 500:
+            print('process sentences ', sentNum)
+
+print('load images done. sentence number ',sentNum)
+
+pkl.dump(features,oupFp_feature)
+print('save file done')
+oupFp_feature.close()
+
